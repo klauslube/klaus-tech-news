471ab7f641a187a4f5efa58a1da09a694296f7bf
klauslube <klauslube93@gmail.com>
2023-01-18 18:14:08 -0300

    feat: create next page function

--- tech_news/scraper.py
+++ tech_news/scraper.py
@@ -31,8 +31,13 @@ def scrape_updates(html_content: str) -> list:
 
 
 # Requisito 3
-def scrape_next_page_link(html_content):
-    """Seu código deve vir aqui"""
+def scrape_next_page_link(html_content: str):
+    selector = Selector(html_content)
+    next_page_url = selector.css("a.next::attr(href)").get()
+    if not next_page_url:
+        return None
+    else:
+        return next_page_url
 
 
 # Requisito 4
